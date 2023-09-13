2f3c4ca8b65b4c7b9d31688e8b6eb0be63fdee56
yongsean <88657089+yongsean@users.noreply.github.com>
2023-09-13 21:07:23 +0800

    ...

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 
-from flask import Flask, render_template, request, session
+from flask import redirect
+import mimetypes
+from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from botocore.exceptions import ClientError
 from pymysql import connections
 import os
 import boto3
@@ -172,6 +175,264 @@ def verifyLogin():
             # User not found, login failed
             return render_template('LoginStudent.html', msg="Access Denied: Invalid Email or Ic Number")
     
+@app.route('/student_home', methods=['GET', 'POST'])
+def student_home():
+    id = session['loggedInStudent']
+
+    select_sql = "SELECT * FROM student WHERE studentId = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(select_sql, (id))
+        student = cursor.fetchone()
+
+        if not student:
+            return "No such student exist."
+
+    except Exception as e:
+        return str(e)
+
+    return render_template('studentHome.html', studentId=student[0],
+                           studentName=student[1],
+                           IC=student[2],
+                           mobileNumber=student[3],
+                           gender=student[4],
+                           address=student[5],
+                           email=student[6],
+                           level=student[7],
+                           programme=student[8],
+                           supervisor=student[9],
+                           cohort=student[10])
+
+# Navigation to Edit Student Page
+@app.route('/edit_student', methods=['GET', 'POST'])
+def edit_student():
+    id = session['loggedInStudent']
+
+    select_sql = "SELECT * FROM student WHERE studentId = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(select_sql, (id))
+        student = cursor.fetchone()
+
+        if not student:
+            return "No such student exist."
+
+    except Exception as e:
+        return str(e)
+
+    pendingRequestCount = check_pending_requests(id)
+
+    return render_template('EditStudentProfile.html', studentId=student[0],
+                           studentName=student[1],
+                           IC=student[2],
+                           mobileNumber=student[3],
+                           gender=student[4],
+                           address=student[5],
+                           email=student[6],
+                           level=student[7],
+                           programme=student[8],
+                           supervisor=student[9],
+                           cohort=student[10],
+                           pendingRequestCount=pendingRequestCount)
+
+# CHECK REQUEST EDIT PENDING
+
+
+def check_pending_requests(id):
+    pending_request_sql = "SELECT COUNT(*) FROM request WHERE studentId = %s AND status = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(pending_request_sql, (id, 'pending'))
+        foundRecords = cursor.fetchall()
+
+        if not foundRecords:
+            return 0
+
+        return foundRecords[0][0]
+
+    except Exception as e:
+        return str(e)
+
+# Update student profile (Function)
+
+
+@app.route('/update_student', methods=['GET', 'POST'])
+def update_student():
+    id = session['loggedInStudent']
+
+    select_sql = "SELECT * FROM student WHERE studentId = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(select_sql, (id))
+        student = cursor.fetchone()
+
+        if not student:
+            return "No such student exist."
+
+    except Exception as e:
+        return str(e)
+
+    # Get the newly updated input fields
+    newStudentName = request.form['studentName']
+    newGender = request.form['gender']
+    newMobileNumber = request.form['mobileNumber']
+    newAddress = request.form['address']
+
+    # Compare with the old fields
+    # Student name
+    if student[1] != newStudentName:
+        # Insert into request table
+        insert_sql = "INSERT INTO request (attribute, `change`, status, reason, studentId) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, ('studentName', newStudentName,
+                                    'pending', None, session['loggedInStudent']))
+        db_conn.commit()
+
+    # Gender
+    if student[4] != newGender:
+        # Insert into request table
+        insert_sql = "INSERT INTO request (attribute, `change`, status, reason, studentId) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, ('gender', newGender, 'pending',
+                                    None, session['loggedInStudent']))
+        db_conn.commit()
+
+    # Mobile number
+    if student[3] != newMobileNumber:
+        # Insert into request table
+        insert_sql = "INSERT INTO request (attribute, `change`, status, reason, studentId) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, ('mobileNumber', newMobileNumber,
+                                    'pending', None, session['loggedInStudent']))
+        db_conn.commit()
+
+    # Address
+    if student[5] != newAddress:
+        # Insert into request table
+        insert_sql = "INSERT INTO request (attribute, `change`, status, reason, studentId) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, ('address', newAddress,
+                                    'pending', None, session['loggedInStudent']))
+        db_conn.commit()
+
+    return redirect('/edit_student')
+
+# Navigate to Upload Resume Page
+
+
+@app.route('/upload_resume', methods=['GET', 'POST'])
+def upload_resume():
+    id = session['loggedInStudent']
+
+    select_sql = "SELECT * FROM student WHERE studentId = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(select_sql, (id))
+        student = cursor.fetchone()
+
+        if not student:
+            return "No such student exist."
+
+    except Exception as e:
+        return str(e)
+
+    return render_template('UploadResume.html', studentId=student[0],
+                           studentName=student[1],
+                           IC=student[2],
+                           mobileNumber=student[3],
+                           gender=student[4],
+                           address=student[5],
+                           email=student[6],
+                           level=student[7],
+                           programme=student[8],
+                           supervisor=student[9],
+                           cohort=student[10],)
+
+# Upload Resume into S3
+
+
+@app.route('/uploadResume', methods=['GET', 'POST'])
+def uploadResume():
+    id = session['loggedInStudent']
+
+    select_sql = "SELECT * FROM student WHERE studentId = %s"
+    cursor = db_conn.cursor()
+    stud_resume_file_name_in_s3 = id + "_resume"
+    student_resume_file = request.files['resume']
+
+    s3 = boto3.resource('s3')
+
+    try:
+        cursor.execute(select_sql, (id))
+        student = cursor.fetchone()
+        db_conn.commit()
+
+        print("Data inserted in MySQL RDS... uploading resume to S3...")
+
+        # Set the content type to 'application/pdf' when uploading to S3
+        s3.Object(custombucket, stud_resume_file_name_in_s3).put(
+            Body=student_resume_file,
+            ContentType='application/pdf'
+        )
+
+        bucket_location = boto3.client(
+            's3').get_bucket_location(Bucket=custombucket)
+        s3_location = (bucket_location['LocationConstraint'])
+
+        if s3_location is None:
+            s3_location = ''
+        else:
+            s3_location = '-' + s3_location
+
+    except Exception as e:
+        return str(e)
+
+    print("Resume uploaded complete.")
+    return render_template('UploadResumeOutput.html', studentName=student[1], id=session['loggedInStudent'])
+
+
+# Download resume from S3 (based on Student Id)
+@app.route('/viewResume', methods=['GET', 'POST'])
+def view_resume():
+    # Retrieve student's ID
+    student_id = session.get('loggedInStudent')
+    if not student_id:
+        return "Student not logged in."
+
+    # Construct the S3 object key
+    object_key = f"{student_id}_resume"
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3')
+
+    try:
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': custombucket,
+                'Key': object_key,
+                'ResponseContentDisposition': 'inline',
+            },
+            ExpiresIn=3600  # Set the expiration time (in seconds) as needed
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchKey':
+            # If the resume does not exist, return a page with a message
+            return render_template('no_resume_found.html')
+        else:
+            return str(e)
+
+    # Redirect the user to the URL of the PDF file
+    return redirect(response)
+
+
+# Navigate to Student View Report
+@app.route('/view_progress_report', methods=['GET', 'POST'])
+def view_progress_report():
+    return render_template('StudentViewReport.html')
+
+# Navigate to Student Registration
 
 # Function to create a database connection context
 def get_db_connection():
@@ -356,6 +617,65 @@ def display_job_details():
 @app.template_filter('replace_and_keep_hyphen')
 def replace_and_keep_hyphen(s):
     return s.replace('-', '<br>-')
+
+
+@app.route('/downloadStudF04', methods=['GET'])
+def download_StudF04():
+    # Construct the S3 object key
+    object_key = f"forms/FOCS_StudF04.docx"
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3')
+
+    try:
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': custombucket,
+                'Key': object_key,
+                'ResponseContentDisposition': 'inline',
+            },
+            ExpiresIn=3600  # Set the expiration time (in seconds) as needed
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchKey':
+            # If the resume does not exist, return a page with a message
+            return render_template('no_resume_found.html')
+        else:
+            return str(e)
+
+    # Redirect the user to the URL of the PDF file
+    return redirect(response)
+
+
+# DOWNLOAD FOCS_StudF05.docx
+@app.route('/downloadStudF05', methods=['GET'])
+def download_StudF05():
+    # Construct the S3 object key
+    object_key = f"forms/FOCS_StudF05.docx"
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3')
+
+    try:
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': custombucket,
+                'Key': object_key,
+                'ResponseContentDisposition': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            },
+            ExpiresIn=3600  # Set the expiration time (in seconds) as needed
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchKey':
+            # If the resume does not exist, return a page with a message
+            return render_template('no_resume_found.html')
+        else:
+            return str(e)
+
+    # Redirect the user to the URL of the PDF file
+    return redirect(response)
             
 
 if __name__ == '__main__':
